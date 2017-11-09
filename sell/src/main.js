aa5e2df4170e76abf4d83774cc83c7bf80c50604
yangjingya <1046042127@qq.com>
2017-11-09 18:49:52 +0800

    simulate an app

---
+++ sell/src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+// import router from './router';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods.vue';
+import seller from './components/seller/seller.vue';
+import ratings from './components/ratings/ratings.vue';
+Vue.use(VueRouter);
+
+// Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// });
+const routes=[
+  {path:'/goods',component:goods},
+  {path:'/seller',component:seller},
+  {path:'/ratings',component:ratings}
+];
+const router=new VueRouter({
+  routes
+});
+export default router;
+router.push('goods');// 相当于页面初始化，显示goods的内容
+
+// 挂载
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  router: router,
+  components: { App }
+});
